0c5f16a1eedf755acf669c37de306ead66f82ccd
Yu Duan <1179730251@qq.com>
2022-06-14 11:19:35 +0800

    fix: fix the bug: photo page is not work

--- src/routes/api/photo/photo-[id].json.js
+++ src/routes/api/photo/photo-[id].json.js
@@ -1,16 +1,16 @@
-import { BASE_URL } from "$lib/variables";
+import { BASE_URL, BASE } from "$lib/variables";
 
 
 
 export async function get(request) {
     const id = request.params.id;
-    const res = await fetch(`${BASE_URL}/api/photographies/${id}?populate=*`);
+    const res = await fetch(`${BASE_URL}/photographies/${id}?populate=*`);
     const response = await res.json();
     const photography = response["data"];
     photography.shootTime = photography.attributes.shootTime;  //拍摄时间
     photography.shootPlace = photography.attributes.shootPlace;//拍摄地点
     photography.title = photography.attributes.title; //文件标题
-    photography.image = "http://121.4.85.24:1337" + photography.attributes.image.data[0].attributes.url;  //博客图像
+    photography.image = BASE + photography.attributes.image.data[0].attributes.url;  //博客图像
     if (photography.attributes.author.data) {
         photography.author = photography.attributes.author.data.attributes.name;  //作者名
     } else {
