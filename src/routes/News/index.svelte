d89860c975fa7f88da326476ff02083189b8a53f
Yu Duan <1179730251@qq.com>
2022-06-30 08:38:34 +0800

    refactor: delete the token price page

--- src/routes/News/index.svelte
+++ src/routes/News/index.svelte
@@ -1,20 +1,5 @@
-<script context="module">
-  export async function load({ fetch }) {
-    let symbol = 'DOTUSDT';
-    const res = await fetch(`https://api.binance.com/api/v3/ticker/price?symbol=DOTUSDT`);
-    const priceInfo = await res.json();
-    return {
-      props: {
-        priceInfo,
-      },
-    };
-  }
-</script>
-
 <script>
   import Breadcrumb from '$lib/component/contentLayout/breadCrumb.svelte';
-  export let priceInfo;
-  let dotPrice = priceInfo.price;
   let breadcrumbInfo = [
     { name: 'Home', url: '/' },
     { name: 'News', url: '' },
@@ -25,7 +10,7 @@
   <Breadcrumb info={breadcrumbInfo} />
   <div class="max-w-screen-xl mx-auto px-8 py-4 sm:px-32 lg:px-48">
     <p class="mt-4 font-bold text-xl dark:text-white">
-      dot价格：{dotPrice}
+      Hello!
     </p>
   </div>
 </sction>
